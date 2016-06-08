73ab1ffb4b3301284b44d73a058c8f35f56fbbde
Etudiant <Etudiant@iMac-de-Etudiant-66.local>
2016-06-08 12:19:22 +0200

    Ajout de la table view

---
+++ StyleShaker/MoodController.swift
@@ -0,0 +1,25 @@
+//
+//  MoodController.swift
+//  StyleShaker
+//
+//  Created by Etudiant on 08/06/2016.
+//  Copyright (c) 2016 esgi. All rights reserved.
+//
+
+import UIKit
+
+class MoodController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- StyleShaker/ViewController.swift
+++ StyleShaker/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var selectHair: UISwitch!
+    @IBOutlet weak var selectSkin: UISwitch!
+    @IBOutlet weak var selectSex: UISwitch!
+    
+    @IBOutlet weak var lightHair: UILabel!
+    @IBOutlet weak var darkHair: UILabel!
+    
+    @IBOutlet weak var lightSkin: UILabel!
+    @IBOutlet weak var darkSkin: UILabel!
+    
+    @IBOutlet weak var maleSex: UILabel!
+    @IBOutlet weak var femaleSex: UILabel!
+
+    @IBAction func validate(sender: AnyObject) {
+        self.performSegueWithIdentifier("toDetail", sender: self);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
